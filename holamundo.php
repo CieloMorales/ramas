3983c84c38015d5e74137506962ed019c7b0636b
CieloMorales <115675392+CieloMorales@users.noreply.github.com>
2024-06-07 10:59:55 -0600

    Update holamundo.php

--- holamundo.php
+++ holamundo.php
@@ -12,4 +12,10 @@ function resta($a, $b) {
 function multiplicacion($a, $b) {
     return $a * $b;
 }
+function division($a, $b) {
+    if ($b == 0) {
+        return "No se puede dividir por cero";
+    }
+    return $a / $b;
+}
 ?>
